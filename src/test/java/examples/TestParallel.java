cc773709f822c1e9344a9214146cf0c9e1be5636
Mark Hughes <m.hughes@heni.com>
2023-03-21 12:06:07 +0000

    working locally

--- src/test/java/examples/TestParallel.java
+++ src/test/java/examples/TestParallel.java
@@ -1,25 +1,28 @@
-// TestParallel.java
-package examples;
-
-import com.intuit.karate.Results;
-import com.intuit.karate.Runner;
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Test;
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
 import net.masterthought.cucumber.Configuration;
 import net.masterthought.cucumber.ReportBuilder;
-import org.apache.commons.io.FileUtils;
 
 public class TestParallel {
 
     @Test
-    void testParallel() {
+    void testParallel() throws IOException {
         Results results = Runner.path("classpath:examples")
                 .outputCucumberJson(true)
-                .parallel(8);
+                .parallel(10);
         generateReport(results.getReportDir());
         assertEquals(0, results.getFailCount(), results.getErrorMessages());
     }
@@ -28,10 +31,27 @@ public class TestParallel {
         Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] { "json" }, true);
         List<String> jsonPaths = new ArrayList<>(jsonFiles.size());
         jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
-        Configuration config = new Configuration(new File("target"), "API Testing - Websites");
+        Configuration config = new Configuration(new File("target"), "Slack Report Repo");
         ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
         reportBuilder.generateReports();
-        PrintKarateReports.printReports();
+        
+        // Copy cucumber report to new directory
+        try {
+            copyCucumberReportToNewDirectory();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void copyCucumberReportToNewDirectory() throws IOException {
+        // Create new directory
+        String projectName = "HWC";
+        String dateStr = new SimpleDateFormat("ddMMyy").format(new Date());
+        File newDir = new File("target/" + projectName + "/" + dateStr);
+        newDir.mkdirs();
+        
+        // Copy files from old directory to new directory
+        FileUtils.copyDirectory(new File("target/cucumber-html-reports"), newDir);
     }
 
 }
---
+++ src/test/java/helpers/slack-report.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+const axios = require('axios');
+
+function createSlackReport(textFilePath, jsonFilePath) {
+  const textData = fs.readFileSync(textFilePath, 'utf8');
+  const jsonData = JSON.parse(textData);
+  const elapsed = (jsonData.elapsedTime / 1000).toFixed(2);
+  const threads = jsonData.threads;
+  const threadTime = (jsonData.totalTime / 1000).toFixed(2);
+  const features = jsonData.featureSummary.length;
+  const skipped = jsonData.featuresSkipped;
+  const efficiency = jsonData.efficiency.toFixed(2);
+  const scenarios = jsonData.scenariosPassed;
+  const passed = jsonData.scenariosPassed;
+  const failed = jsonData.featuresFailed;
+
+  const baseUrl = 'https://karate-tests.no-zero.net/';
+  const date = new Date();
+  const day = date.getDate().toString().padStart(2, '0');
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const year = date.getFullYear().toString().substr(-2);
+  const dateCode = `${day}${month}${year}`;
+  const url = baseUrl + 'HWC/' + dateCode + '/overview-features.html';
+
+  const slackText = `elapsed:   ${elapsed} | threads:   ${threads} | thread time: ${threadTime}\nfeatures:     ${features} | skipped:   ${skipped} | efficiency: ${efficiency}\nscenarios:    ${scenarios} | passed:     ${passed} | failed: ${failed}`;
+
+  const slackBlock = 
+  {
+    "type": "section",
+    "text": {
+      "type": "mrkdwn",
+      "text": slackText
+    },
+    "accessory": {
+      "type": "button",
+      "text": {
+        "type": "plain_text",
+        "text": "View Report"
+      },
+      "url": url
+    }
+  }
+
+  const slackJsonData = {
+    "blocks": [slackBlock]
+  };
+
+  fs.writeFileSync(jsonFilePath, JSON.stringify(slackJsonData, null, 2), 'utf8');
+
+  axios.post('https://hooks.slack.com/services/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', slackJsonData)
+    .then(res => console.log('Slack notification sent successfully'))
+    .catch(err => console.error('Failed to send Slack notification:', err));
+}
+
+const textFilePath = 'target/karate-reports/karate-summary-json.txt';
+const jsonFilePath = 'target/karate-reports/slack-block.json';
+createSlackReport(textFilePath, jsonFilePath);
